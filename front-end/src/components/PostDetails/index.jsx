160b0c713d72bdc421b02b39248c799f14d05c5e
Davi Souza <davissbf@gmail.com>
2021-04-02 00:43:34 -0300

    listing posts

--- front-end/src/components/PostDetails/index.jsx
+++ front-end/src/components/PostDetails/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function PostDetails() {
-  return <div></div>;
-}
+const PostDetails = () => {
+  return <></>;
+};
 
 export default PostDetails;
--- front-end/src/components/PostsList/index.jsx
+++ front-end/src/components/PostsList/index.jsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useSelector } from 'react-redux';
+import { Grid, Button } from '@material-ui/core';
+import Post from '../Post/index';
+import gridFour from '../../assets/grid_four.svg';
+import gridThree from '../../assets/grid_three.svg';
 
-const PostsList = () => {
-  return <div>PostList</div>;
+const useStyles = makeStyles((theme) => ({
+  layoutShifter: {
+    float: 'right',
+    margin: theme.spacing(2),
+  },
+}));
+
+const PostDetails = () => {
+  const posts = useSelector((state) => state.posts.posts);
+
+  const [layout, setLayout] = useState('gridThree');
+
+  const calculateMd = () => {
+    return layout === 'gridThree' ? 4 : 3;
+  };
+
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.layoutShifter}>
+        <Button
+          variant="text"
+          size="small"
+          onClick={() => setLayout('gridThree')}
+        >
+          <img
+            src={gridThree}
+            style={{ background: layout === 'gridThree' ? '#ccc' : '' }}
+            alt="Three Columns Grid Icon"
+          />
+        </Button>
+        <Button
+          variant="text"
+          size="small"
+          onClick={() => setLayout('gridFour')}
+        >
+          <img
+            src={gridFour}
+            style={{ background: layout === 'gridFour' ? '#ccc' : '' }}
+            alt="Four Columns Grid Icon"
+          />
+        </Button>
+      </div>
+      <Grid container spacing={2} alignContent="stretch">
+        {posts.length > 0 &&
+          posts.map((post) => (
+            <Grid item key={post?._id} xs={12} md={calculateMd()}>
+              <Post {...post} />
+            </Grid>
+          ))}
+      </Grid>
+    </>
+  );
 };
 
-export default PostsList;
+export default PostDetails;
